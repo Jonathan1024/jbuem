4498f9525c2fdc242afb23e1cc1749bf5c75a3a5
jonathan <jnewton2014@gmail.com>
2016-01-26 15:31:49 -0600

    daq.jbu.edu now takes the user to a static web page.

--- MainSite/views.py
+++ MainSite/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("This is our new web page!")
+    message = 'Hello Harrison!'
+    amount = 100
+    context = {
+        'message':message,
+        'amount':amount,
+    }
+    return render(request, 'MainSite/index.html', context)
