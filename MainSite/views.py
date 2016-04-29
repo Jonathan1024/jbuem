706e268cda083c97397048dafda8b2a88f322728
jonathan <jnewton2014@gmail.com>
2016-04-29 09:04:05 -0500

    Added in the sum totals for dashboard.

--- MainSite/views.py
+++ MainSite/views.py
@@ -5,7 +5,8 @@ from django.db import connection
 import csv
 from django.utils.six.moves import range
 from django.http import StreamingHttpResponse
-from MainSite .models import Solar, Meter, Pyranometer, Wind
+from MainSite .models import Solar, Meter, Pyranometer, \
+    Wind, EnphaseTotals, FroniusTotals, WindTotals, MeterTotals
 import json
 # Create your views here.
 
@@ -79,6 +80,10 @@ def dashboard1(request):
     meter = Meter.objects.order_by('-time_stamp')[0]
     solar = Solar.objects.order_by('-time_stamp')[0]
     wind = Wind.objects.order_by('-time_stamp')[0]
+    enphase_totals = EnphaseTotals.objects.order_by('-time_stamp')[0]
+    fronius_totals = FroniusTotals.objects.order_by('-time_stamp')[0]
+    grid_totals = WindTotals.objects.order_by('-time_stamp')[0]
+    # wind_totals = EnphaseTotals.objects.order_by('-time_stamp')[0]
 
 
 
@@ -87,6 +92,18 @@ def dashboard1(request):
         'current_solar_fronius': solar.fronius_power,
         'current_wind': wind.wind_power,
         'current_grid': meter.btc_power,
+        'daily_enphase':enphase_totals.daily_total,
+        'weekly_enphase': enphase_totals.weekly_total,
+        'monthly_enphase': enphase_totals.monthly_total,
+        'yearly_enphase': enphase_totals.yearly_total,
+        'daily_fronius':fronius_totals.daily_total,
+        'weekly_fronius': fronius_totals.weekly_total,
+        'monthly_fronius': fronius_totals.monthly_total,
+        'yearly_fronius': fronius_totals.yearly_total,
+        'daily_grid':grid_totals.daily_total,
+        'weekly_grid': grid_totals.weekly_total,
+        'monthly_grid': grid_totals.monthly_total,
+        'yearly_grid': grid_totals.yearly_total,
     }
 
     return render(request, 'MainSite/dashboard1.html', context)
